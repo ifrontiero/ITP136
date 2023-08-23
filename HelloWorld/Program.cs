35b8983b3706e701f356f46788c02913cefc5a1f
Ian Frontiero <itf81834@email.vccs.edu>
2023-08-23 19:20:11 -0400

    week 1

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld
+{
+    using static System.Console;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("What is your name?");
+
+            Write("> ");
+
+            string userName = ReadLine();
+
+            WriteLine("Your name is " + userName);
+        }
+    }
+}
