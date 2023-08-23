474a5740db25d4e386fc37302ae60b98dddb6fcc
Ian Frontiero <itf81834@email.vccs.edu>
2023-08-23 19:31:14 -0400

    week 1

---
+++ Week1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld
+{
+    using static System.Console;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Who is being seen today?");
+
+            Write("> ");
+
+            string patientName = ReadLine();
+            
+            WriteLine("Welcome: " + patientName);
+        }
+    }
+}
