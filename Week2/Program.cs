3e6e89b89d81b46a62386db11ba0e8f36ad41ada
Ian Frontiero <itf81834@email.vccs.edu>
2023-09-01 17:54:56 -0400

    week 2

---
+++ Week2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Earl's Garage \n");
+
+            string? inputString;
+
+            // Oil change
+            Console.Write("Enter oil change price: ");
+            inputString = Console.ReadLine();
+
+            double oilChangePrice = Convert.ToDouble(inputString);
+
+            // Tires
+            Console.Write("Enter tire price: ");
+            inputString = Console.ReadLine();
+
+            double tirePrice = Convert.ToDouble(inputString);
+
+            // Inspection
+            Console.Write("Enter inspection price: ");
+            inputString = Console.ReadLine();
+
+            double inspectionPrice = Convert.ToDouble(inputString);
+
+            // Calculate Subtotal
+            double subtotal = oilChangePrice + tirePrice + inspectionPrice;
+            Console.WriteLine("Subtotal: " + subtotal.ToString("C"));
+
+            // Calculate tax
+            double tax = subtotal * 0.06;
+            Console.WriteLine("Tax: " + tax.ToString("C"));
+
+            // Ouput total
+            double total = subtotal + tax;
+            Console.WriteLine("Total: " + total.ToString("C"));
+        }
+    }
+}
